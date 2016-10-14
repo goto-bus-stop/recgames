3fdc1301729441fd067772d6d49954b6b4bebd41
René Kooi <renee@kooi.me>
2016-10-14 14:05:59 +0200

    add RecordedGame model

---
+++ database/migrations/2016_10_14_112124_create_recorded_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordedGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recorded_games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('filename');
+            $table->string('path')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recorded_games');
+    }
+}
