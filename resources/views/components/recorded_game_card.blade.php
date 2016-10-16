57996edb08a77efbdf002cc4631b843f4a88abc8
René Kooi <renee@kooi.me>
2016-10-16 17:10:27 +0200

    put links to recorded games in their card footer

--- resources/views/components/recorded_game_card.blade.php
+++ resources/views/components/recorded_game_card.blade.php
@@ -9,11 +9,15 @@
     </div>
     <div class="card-content">
       <p>{{ $rec->filename }}</p>
-      <p>
-        <time datetime="{{ $rec->created_at->toW3CString() }}" title="{{ $rec->created_at }}">
-          {{ $rec->created_at->diffForHumans() }}
-        </time>
-      </p>
+    </div>
+    <div class="card-footer">
+      <time class="card-footer-item"
+            datetime="{{ $rec->created_at->toW3CString() }}"
+            title="{{ $rec->created_at }}">
+        {{ $rec->created_at->diffForHumans() }}
+      </time>
+      <a class="card-footer-item" href="{{ action('GamesController@show', $rec->slug) }}">View</a>
+      <a class="card-footer-item" href="{{ url('unimplemented') }}">Download</a>
     </div>
   </div>
 </div>
