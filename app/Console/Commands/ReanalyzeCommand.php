bed96f0ab96d196911ec563c31177c3b867de392
René Kooi <renee@kooi.me>
2016-11-08 13:10:46 +0100

    add artisan command to reanalyze every game

---
+++ app/Console/Commands/ReanalyzeCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\RecordedGame;
+use App\Jobs\RecAnalyzeJob;
+
+class ReanalyzeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'recgames:reanalyze';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Queue reanalysis of all games.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach (RecordedGame::cursor() as $game) {
+            dispatch(new RecAnalyzeJob($game));
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 use App\Jobs\PurgeUnusedGamesJob;
+use App\Console\Commands\ReanalyzeCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        ReanalyzeCommand::class,
     ];
 
     /**
