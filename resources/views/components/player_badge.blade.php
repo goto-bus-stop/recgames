ca69c684a0b980f8636a05611536cc3e6abbf508
René Kooi <renee@kooi.me>
2016-12-06 17:13:02 +0100

    Remove icon from player badges with invalid civilizations

--- resources/views/components/player_badge.blade.php
+++ resources/views/components/player_badge.blade.php
@@ -1,6 +1,10 @@
 <span class="tag player-badge is-player-{{ $color }}-bg">
-  <img class="civ-icon"
-       src="{{ asset('vendor/recanalyst/civs/' . $color . '/' . $civilization . '.png') }}"
-       alt="@lang('recanalyst::ageofempires.civilizations.' . $civilization)">
+  @if ($civilization < 0)
+    <div class="civ-icon">&nbsp;</div>
+  @else
+    <img class="civ-icon"
+        src="{{ asset('vendor/recanalyst/civs/' . $color . '/' . $civilization . '.png') }}"
+        alt="@lang('recanalyst::ageofempires.civilizations.' . $civilization)">
+  @endif
   {{ $name }}
 </span>
