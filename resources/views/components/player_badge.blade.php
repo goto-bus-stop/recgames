a41161b30cd2c5aec0d0e8622e1a12f01b3efa38
René Kooi <renee@kooi.me>
2016-12-06 18:59:58 +0100

    Change player badge to component-style CSS

--- resources/views/components/player_badge.blade.php
+++ resources/views/components/player_badge.blade.php
@@ -1,8 +1,8 @@
-<span class="tag player-badge is-player-{{ $color }}-bg">
+<span class="PlayerBadge is-player-{{ $color }}-bg">
   @if ($civilization < 0)
-    <div class="civ-icon">&nbsp;</div>
+    <div class="PlayerBadge-civIcon">&nbsp;</div>
   @else
-    <img class="civ-icon"
+    <img class="PlayerBadge-civIcon"
          src="{{ asset('vendor/recanalyst/civs/' . $color . '/' . $civilization . '.png') }}"
          alt="@lang('recanalyst::ageofempires.civilizations.' . $civilization)"
          title="@lang('recanalyst::ageofempires.civilizations.' . $civilization)">
