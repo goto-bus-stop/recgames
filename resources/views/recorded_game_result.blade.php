601d163dd3c010a0cfb2f48191023820a8d59f87
René Kooi <renee@kooi.me>
2016-12-08 13:27:25 +0100

    Add Open Graph metadata to recorded game pages.

--- resources/views/recorded_game_result.blade.php
+++ resources/views/recorded_game_result.blade.php
@@ -2,6 +2,14 @@
 
 @section('title', $title)
 
+@push('head')
+  <meta property="og:url"         content="{{ action('GamesController@show', $rec->slug) }}">
+  <meta property="og:type"        content="article">
+  <meta property="og:title"       content="{{ $title }}">
+  <meta property="og:image"       content="{{ asset($rec->minimap_url) }}">
+  <meta property="og:image:type"  content="image/png">
+@endpush
+
 @section('content')
     {!! $html !!}
 @endsection
