aa10aa75930319b5076dd1e8d46b9b3108058fc5
René Kooi <renee@kooi.me>
2016-12-09 12:08:48 +0100

    Add Disqus comments to recorded game pages.

--- resources/views/recorded_game_result.blade.php
+++ resources/views/recorded_game_result.blade.php
@@ -1,15 +1,36 @@
+@inject('helpers', 'App\Services\BladeHelpersService')
+
 @extends('layouts.main')
 
 @section('title', $title)
 
 @push('head')
   <meta property="og:url"         content="{{ action('GamesController@show', $rec->slug) }}">
-  <meta property="og:type"        content="article">
+  <meta property="og:type"        content="website">
   <meta property="og:title"       content="{{ $title }}">
   <meta property="og:image"       content="{{ asset($rec->minimap_url) }}">
   <meta property="og:image:type"  content="image/png">
+  <meta property="og:site_name"   content="recgam.es">
 @endpush
 
 @section('content')
-    {!! $html !!}
+  {!! $html !!}
+
+  <section class="section">
+    <div class="container" id="disqus_thread"></div>
+  </section>
+  <script>
+    var disqus_config = function () {
+      this.page.title = {!! json_encode($title) !!};
+      this.page.url = {!! json_encode(action('GamesController@show', $rec->slug)) !!};
+      this.page.identifier = {!! json_encode($rec->slug) !!};
+    };
+    (function() {
+      var d = document, s = d.createElement('script');
+      s.src = '//recgames.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
+  </script>
+  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
