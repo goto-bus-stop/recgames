61bf3e2280d587ba7da4b239ce28c21d7abffd51
René Kooi <renee@kooi.me>
2017-01-17 15:29:27 +0100

    Move recorded game card styles into CSS component.

--- resources/views/components/recorded_game_card.blade.php
+++ resources/views/components/recorded_game_card.blade.php
@@ -1,14 +1,14 @@
 @inject('helpers', 'App\Services\BladeHelpersService')
 
 <div class="column">
-  <div class="card" style="margin: auto">
-    <div class="card-image">
+  <div class="RecordedGameCard" style="margin: auto">
+    <div class="RecordedGameCard-main">
       <a href="{{ action('GamesController@show', $rec->slug) }}">
-        <figure class="image is-2by1">
+        <figure class="RecordedGameCard-image">
           <img src="{{ asset($rec->minimap_url) }}" alt="">
         </figure>
       </a>
-      <div class="card-hover-data">
+      <div class="RecordedGameCard-data">
         <p>
           <strong>Version</strong>
           @lang('recanalyst::game_versions.' . $rec->analysis->game_version)
@@ -38,17 +38,17 @@
         </div>
       </div>
     </div>
-    <div class="card-content">
+    <div class="RecordedGameCard-name">
       <p>{{ $rec->filename }}</p>
     </div>
-    <div class="card-footer">
-      <time class="card-footer-item"
+    <div class="RecordedGameCard-footer">
+      <time class="RecordedGameCard-time"
             datetime="{{ $rec->created_at->toW3CString() }}"
             title="{{ $rec->created_at }}">
         {{ $rec->created_at->diffForHumans() }}
       </time>
-      <a class="card-footer-item" href="{{ action('GamesController@show', $rec->slug) }}">View</a>
-      <a class="card-footer-item" href="{{ action('GamesController@download', $rec->slug) }}">Download</a>
+      <a class="RecordedGameCard-link" href="{{ action('GamesController@show', $rec->slug) }}">View</a>
+      <a class="RecordedGameCard-link" href="{{ action('GamesController@download', $rec->slug) }}">Download</a>
     </div>
   </div>
 </div>
