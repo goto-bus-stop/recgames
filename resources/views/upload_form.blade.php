b6fd36bf489b27590bd84ec1f74b49cef61f0b33
Renée Kooi <renee@kooi.me>
2017-10-20 16:19:23 +0200

    Update recorded game file upload form for new bulma.

--- resources/views/upload_form.blade.php
+++ resources/views/upload_form.blade.php
@@ -14,25 +14,38 @@
       </div>
     </div>
   @endif
+
   <div class="section">
     <form action="{{ action('GamesController@upload') }}"
           method="POST"
           enctype="multipart/form-data"
           class="container"
+          style="max-width: 800px"
           id="upload-form">
       {{ csrf_field() }}
-      <label class="label">Recorded Game File</label>
-      <p class="control">
-        <input class="input" type="file" name="recorded_game[]" id="upload-file" multiple>
-      </p>
-      <p class="control">
-        <button class="button is-primary" type="submit" id="upload-button">
-          Upload
-        </button>
-      </p>
-      <p class="control is-hidden">
-        <progress class="progress" id="upload-progress"></progress>
-      </p>
+      <div class="field">
+        <label class="label">Recorded Game File</label>
+        <p class="control">
+          <input class="input"
+                 type="file"
+                 name="recorded_game[]"
+                 id="upload-file"
+                 multiple>
+        </p>
+        <span class="help">Files can be recorded games, or zip archives containing recorded games.</span>
+      </div>
+      <div class="field">
+        <p class="control">
+          <button class="button is-primary" type="submit" id="upload-button">
+            Upload
+          </button>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control is-hidden">
+          <progress class="progress" id="upload-progress"></progress>
+        </p>
+      </div>
     </form>
   </div>
 @endsection
